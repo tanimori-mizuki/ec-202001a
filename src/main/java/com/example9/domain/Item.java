9480bb995e8fa58412bec646fa4c98a444d79770
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-03-02 15:18:47 +0900

    itemとToppingドメインを作成しました。

---
+++ src/main/java/com/example9/domain/Item.java
@@ -0,0 +1,86 @@
+package com.example9.domain;
+
+import java.util.List;
+
+public class Item {
+
+	private Integer id;
+	private String name;
+	private String description;
+	private Integer priceM;
+	private Integer priceL;
+	private String imagePath;
+	private Boolean deleted;
+	private List<Topping> toppingList;
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", description=" + description + ", priceM=" + priceM + ", priceL="
+				+ priceL + ", imagePath=" + imagePath + ", deleted=" + deleted + ", toppingList=" + toppingList + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPriceM() {
+		return priceM;
+	}
+
+	public void setPriceM(Integer priceM) {
+		this.priceM = priceM;
+	}
+
+	public Integer getPriceL() {
+		return priceL;
+	}
+
+	public void setPriceL(Integer priceL) {
+		this.priceL = priceL;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public List<Topping> getToppingList() {
+		return toppingList;
+	}
+
+	public void setToppingList(List<Topping> toppingList) {
+		this.toppingList = toppingList;
+	}
+
+}
---
+++ src/main/java/com/example9/domain/Topping.java
@@ -0,0 +1,47 @@
+package com.example9.domain;
+
+public class Topping {
+
+	private Integer id;
+	private String name;
+	private Integer priceM;
+	private Integer priceL;
+
+	@Override
+	public String toString() {
+		return "Topping [id=" + id + ", name=" + name + ", priceM=" + priceM + ", priceL=" + priceL + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPriceM() {
+		return priceM;
+	}
+
+	public void setPriceM(Integer priceM) {
+		this.priceM = priceM;
+	}
+
+	public Integer getPriceL() {
+		return priceL;
+	}
+
+	public void setPriceL(Integer priceL) {
+		this.priceL = priceL;
+	}
+
+}
