727a112955559a57f81011a2299c39bcbeaa0626
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-03-02 15:57:45 +0900

    ログイン機能を新規作成

--- src/main/java/com/example9/domain/User.java
+++ src/main/java/com/example9/domain/User.java
@@ -1,13 +1,25 @@
 package com.example9.domain;
 
-public class user {
+/**
+ * ユーザー情報を表すドメイン.
+ * @author mayumiono
+ *
+ */
+public class User {
 
+	/** ユーザーID */
 	private Integer id;
+	/** ユーザー名 */
 	private String name;
+	/** メールアドレス */
 	private String email;
+	/** パスワード */
 	private String password;
+	/** 郵便番号 */
 	private String zipcode;
+	/** 住所 */
 	private String address;
+	/** 電話番号 */
 	private String telephone;
 
 	@Override
---
+++ src/main/java/com/example9/form/LoginForm.java
@@ -0,0 +1,37 @@
+package com.example9.form;
+
+/**
+ * ログインフォームへの入力情報
+ * 
+ * @author mayumiono
+ *
+ */
+public class LoginForm {
+
+	/** メールアドレス */
+	private String email;
+	/** パスワード */
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginForm [email=" + email + ", password=" + password + "]";
+	}
+
+}
