a21ab5fc88b43590ee8f354030a28768111714ab
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-03-02 14:45:40 +0900

    first commit

---
+++ src/main/java/com/example9/Ec202001aApplication.java
@@ -0,0 +1,13 @@
+package com.example9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202001aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202001aApplication.class, args);
+	}
+
+}
