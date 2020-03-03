33e09d1baf9f5cfc71b66d7b6100c8a5cdfb8596
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-03-03 18:19:46 +0900

    findByUserIdAndStatus()追加

---
+++ src/main/java/com/example9/domain/Order.java
@@ -0,0 +1,150 @@
+package com.example9.domain;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 注文を表すドメインクラス
+ * 
+ * @author yuuki
+ *
+ */
+public class Order {
+	/** 主キー */
+	private Integer id;
+	/** ユーザid */
+	private Integer userId;
+	/** 状態 */
+	private Integer status;
+	/** 合計金額 */
+	private Integer totalPrice;
+	/** 注文日 */
+	private Date orderDate;
+	/** 宛先氏名 */
+	private String destinationName;
+	/** 宛先Eメール */
+	private String destinationEmail; 
+	/** 宛先郵便番号 */
+	private String destinationZipcode;
+	/** 宛先住所 */
+	private String destinationAddress;
+	/** 宛先電話番号 */
+	private String destinationTel;
+	/** 配達時間 */
+	private Timestamp deliveryTime;
+	/** 支払方法 */
+	private Integer paymentMethod;
+	/** ユーザ */
+	private User user;
+	/** 注文商品リスト */
+	private List <OrderItem> orderItemList;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getDestinationName() {
+		return destinationName;
+	}
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+	public String getDestinationEmail() {
+		return destinationEmail;
+	}
+	public void setDestinationEmail(String destinationEmail) {
+		this.destinationEmail = destinationEmail;
+	}
+	public String getDestinationZipcode() {
+		return destinationZipcode;
+	}
+	public void setDestinationZipcode(String destinationZipcode) {
+		this.destinationZipcode = destinationZipcode;
+	}
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+	public String getDestinationTel() {
+		return destinationTel;
+	}
+	public void setDestinationTel(String destinationTel) {
+		this.destinationTel = destinationTel;
+	}
+	public Timestamp getDeliveryTime() {
+		return deliveryTime;
+	}
+	public void setDeliveryTime(Timestamp deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+	public Integer getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(Integer paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<OrderItem> getOrderItemList() {
+		return orderItemList;
+	}
+	public void setOrderItemList(List<OrderItem> orderItemList) {
+		this.orderItemList = orderItemList;
+	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", userId=" + userId + ", status=" + status + ", totalPrice=" + totalPrice
+				+ ", orderDate=" + orderDate + ", destinationName=" + destinationName + ", destinationEmail="
+				+ destinationEmail + ", destinationZipcode=" + destinationZipcode + ", destinationAddress="
+				+ destinationAddress + ", destinationTel=" + destinationTel + ", deliveryTime=" + deliveryTime
+				+ ", paymentMethod=" + paymentMethod + ", user=" + user + ", orderItemList=" + orderItemList + "]";
+	} 
+	// getTax()メソッドを実装予定
+	public int getTax() {
+		int tax = getCalcTotalPrice() * 10 / 100;
+		return tax;
+	}
+	
+	
+	// getCalcTotalPrice()メソッドを実装予定
+	public int getCalcTotalPrice() {
+		int totalPrice = 0;
+		for (OrderItem orderItem : orderItemList) {
+			totalPrice += orderItem.getSubTotal();
+		}
+		return totalPrice;
+	}
+}
---
+++ src/main/java/com/example9/domain/OrderItem.java
@@ -0,0 +1,91 @@
+package com.example9.domain;
+
+import java.util.List;
+
+/**
+ * 注文商品を表すドメインクラス.
+ * 
+ * @author yuuki
+ *
+ */
+public class OrderItem {
+	/** 主キー */
+	private Integer id;
+	/** 商品id */
+	private Integer itemId;
+	/** 注文id */
+	private Integer orderId;
+	/** 数量 */
+	private Integer quantity;
+	/** サイズ */
+	private Character size;
+	/** 商品 */
+	private Item item;
+	/** 注文トッピングリスト */
+	private List <OrderTopping> orderToppingList;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getItemId() {
+		return itemId;
+	}
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Character getSize() {
+		return size;
+	}
+	public void setSize(Character size) {
+		this.size = size;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public List<OrderTopping> getOrderToppingList() {
+		return orderToppingList;
+	}
+	public void setOrderToppingList(List<OrderTopping> orderToppingList) {
+		this.orderToppingList = orderToppingList;
+	}
+	@Override
+	public String toString() {
+		return "OrderItem [id=" + id + ", itemId=" + itemId + ", orderId=" + orderId + ", quantity=" + quantity
+				+ ", size=" + size + ", item=" + item + ", orderToppingList=" + orderToppingList + "]";
+	}
+	
+	/**
+	 * 注文商品の小計を求める.
+	 * 
+	 * @return 小計
+	 */
+	public int getSubTotal() {
+		int subTotalPrice = 0;
+		
+		if (size == 'M') {
+			subTotalPrice = (item.getPriceM() + 200 * orderToppingList.size()) * quantity;
+		} else if (size == 'L') {
+			subTotalPrice = (item.getPriceL() + 300 * orderToppingList.size()) * quantity;
+		}
+		return subTotalPrice;
+	}
+	
+}
