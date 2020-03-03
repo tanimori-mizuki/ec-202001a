ab84d86bf7af450a21bca8fdf712191640dd70d0
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-03-03 11:38:13 +0900

    各HTML、css,img,jsファイルのパスを修正

--- src/main/java/com/example9/controller/LoginLogoutController.java
+++ src/main/java/com/example9/controller/LoginLogoutController.java
@@ -78,7 +78,7 @@ public class LoginLogoutController {
 	// ToDo: ShowItemListController完成次第削除
 	@RequestMapping("/test")
 	public String showCartLost() {
-		return "item_list_curry";
+		return "register_user";
 	}
 
 	/**
