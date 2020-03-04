90783ed0e879f7a5dfc7e7e81eee673e5ff73996
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-03-04 15:36:17 +0900

    JQueryが動作しなくなっていたので修正

--- src/main/resources/static/js/item_detail.js
+++ src/main/resources/static/js/item_detail.js
@@ -15,19 +15,13 @@ $(function() {
 
 	// 値段の計算をして変更する関数
 	function calc_price() {
-		var size = $("input[name='responsibleCompany']:checked").val();
-		console.log("サイズ");
-		console.log(size);
+		var size = $("input:radio:checked").val();
 		var topping_count = $("input:checkbox:checked").length;
-		console.log("トッピング数");
-		console.log(topping_count);
-		var curry_num = $("select[name='area'] option:selected").val();
+		var curry_num = $("select[name='quantity'] option:selected").val();
 		if (size == "M") {
-			console.log("Msize分岐");
 			var size_price = $("#priceSizeM").text().replace(/,/g,'')*1;
 			var topping_price = $("#toppingUnitPriceSizeM").text() * topping_count;
 		} else {
-			console.log("Lsize分岐");
 			var size_price = $("#priceSizeL").text().replace(/,/g,'')*1;
 			var topping_price = $("#toppingUnitPriceSizeL").text() * topping_count;
 		}
